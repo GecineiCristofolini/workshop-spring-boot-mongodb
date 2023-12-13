716931118214da3a31f5fb77fd6fa9f6f1c4fc6d
GecineiCristofolini <gecinei.c@hotmail.com>
2023-12-13 13:30:31 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/gecineicristofolini/workshopmongo/domain/User.java
@@ -0,0 +1,64 @@
+package com.gecineicristofolini.workshopmongo.domain;
+
+import java.util.Objects;
+
+public class User {
+	
+	private String Id;
+	private String name;
+	private String email;
+	
+	public User() {
+		
+	}
+
+	public User(String id, String name, String email) {
+		super();
+		Id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(Id, other.Id);
+	}
+	
+	
+}
