2d8d6c0289ede5d85c3d30b8202ee977f5a68148
GecineiCristofolini <gecinei.c@hotmail.com>
2023-12-18 08:38:59 -0300

    Referenciando os posts do usuario

--- src/main/java/com/gecineicristofolini/workshopmongo/domain/User.java
+++ src/main/java/com/gecineicristofolini/workshopmongo/domain/User.java
@@ -1,7 +1,10 @@
 package com.gecineicristofolini.workshopmongo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")
@@ -11,6 +14,9 @@ public class User {
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {
 		
@@ -50,6 +56,14 @@ public class User {
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
